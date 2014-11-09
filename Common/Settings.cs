8be4a9cff6577e1dec33a34ce4b212c0ff1a4bce
Kevin Aenmey <kevpublic@gmail.com>
2014-11-09 12:57:02 -0500

    Remove sensitive information

--- Common/Settings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Common
-{
-    public static class Settings
-    {
-        public const string CONNECTION_STRING_SBMP = "Endpoint=sb://aenmey.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=7+p7yKNF0vzMVGzabKURRqEk71YaHaaoG6dAZTezrWo=";
-        public const string CONNECTION_STRING_AMQP = "Endpoint=sb://aenmey.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=7+p7yKNF0vzMVGzabKURRqEk71YaHaaoG6dAZTezrWo=;TransportType=Amqp";
-    } 
-}
